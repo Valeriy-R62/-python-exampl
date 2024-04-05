1cb568fda4edf1f96b2165f3467fa198ee9f9680
Valeriy-R62 <165371674+Valeriy-R62@users.noreply.github.com>
2024-04-05 18:12:52 +0500

    Add files via upload

---
+++ homeworke4.py
@@ -0,0 +1,14 @@
+immutable_var=32,"B12",True
+print(immutable_var)
+#immutable_var[2]=False
+mutable_list=['JM',47,'E2',"G_7"]
+print(mutable_list)
+mutable_list[0]="bmv"
+print(mutable_list)
+mutable_list.append('cola')
+print(mutable_list)
+mutable_list.extend('kilt')
+print(mutable_list)
+mutable_list.remove('E2')
+print(mutable_list)
+print('cola' in mutable_list)
