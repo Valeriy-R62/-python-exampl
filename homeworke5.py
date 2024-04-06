97dd9823767f8c38160dd7f156ea6f820b57d34d
Valeriy-R62 <165371674+Valeriy-R62@users.noreply.github.com>
2024-04-06 14:23:27 +0500

    Add files via upload

---
+++ homeworke5.py
@@ -0,0 +1,15 @@
+my_list=["colle",'dog','шпиц','taxa']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+my_list.append("mops")
+print(my_list[2:6])
+my_list[2]='fox'
+print(my_list)
+my_dict={'Bob':46,'Jon':18,'Will':65}
+print(my_dict)
+print(my_dict['Jon'])
+my_dict['Jon']=18-3
+print(my_dict['Jon'])
+my_dict["Kat"]=55
+print(my_dict)
