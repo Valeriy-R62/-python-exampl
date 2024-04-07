033e306bc985d3f9dae7dacf99816bea2b559d93
Valeriy-R62 <165371674+Valeriy-R62@users.noreply.github.com>
2024-04-07 19:16:38 +0500

    Update homeworke5.py  исправленный

--- homeworke5.py
+++ homeworke5.py
@@ -1,15 +1,14 @@
-my_list = ["colle",'dog','шпиц','taxa']
-    print(my_list)
-        print(my_list[0])
-    print(my_list[-1])
-my_list.append("mops")
-    print(my_list[2:6])
+my_list = ["colle",'dog','шпиц','taxa',"mops"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:6])
 my_list[2]='fox'
-    print(my_list)
+print(my_list)
 my_dict = {'Bob':46,'Jon':18,'Will':65}
-    print(my_dict)
-        print(my_dict['Jon'])
-my_dict['Jon'] = 18-3
-    print(my_dict['Jon'])
+print(my_dict)
+print(my_dict['Jon'])
+my_dict['Jon'] = 77
+print(my_dict['Jon'])
 my_dict["Kat"] = 55
-    print(my_dict)
+print(my_dict)
