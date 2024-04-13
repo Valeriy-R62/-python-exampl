d9515eee47b57f14551f511123357e6896f182c8
Valeriy-R62 <165371674+Valeriy-R62@users.noreply.github.com>
2024-04-13 13:11:11 +0500

    Add files via upload

---
+++ homeworke10.py
@@ -0,0 +1,7 @@
+moi_avto = ['BMW','MB','LADA','KIA','HONDA']
+for i in moi_avto:
+    print("я езжу на автомобиле марки ",i)
+cars_count = int(0)
+for i in moi_avto:
+    cars_count += 10
+    print(cars_count)
