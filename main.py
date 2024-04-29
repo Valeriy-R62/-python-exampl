3374bc0ba1233d753293f8ff9cdfd5777e865132
Valeriy-R62 <165371674+Valeriy-R62@users.noreply.github.com>
2024-04-29 15:15:05 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-def test(*params):
-    print(params)
-
-
-test(2, False, 'крот')
-def faktorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * faktorial(n - 1)
-
-
-print(faktorial(9))
+import modul_1
+modul_1.privet_1()
+modul_1.privet_2()
